c3ae2165022ded00f3501ea3ade0eb1b11317624
wolf <wolf@x.apy-paradise.locale>
2024-03-27 18:58:18 +0100

    add trainning file download to detector.py

--- scapy/detector.py
+++ scapy/detector.py
@@ -8,18 +8,19 @@ libopencv-dev, python3-opencv, python3-numpy python3-scipy
 training files:
 http://eclecti.cc/files/2008/03/haarcascade_frontalface_alt.xml
 Improvement done:
+- auto download training files
 
 Improvement to do:
 - add argparse for folders
-- auto download training files
 
 """
 
 import cv2
 import os
+import requests
 
 
-def detect(srcdir, tgdir, train_dir):
+def detect(srcdir: str, tgdir: str, train_dir: str) -> None:
     for fname in os.listdir(srcdir):
         if not fname.upper().endswith('.JPEG'):
             continue
@@ -45,5 +46,23 @@ def detect(srcdir, tgdir, train_dir):
         cv2.imwrite(newname, img)
 
 
+def downloadTraining(fileUrl: str, path: str) -> str:
+    """
+    This function will dl a file from URL and store it in path
+    :param fileUrl: the url for file dl
+    :param path: the path where we want to store file
+    :return: log of operation
+    """
+    response = requests.get(fileUrl)
+    destFile = os.path.join(path, fileUrl.split('/')[-1])
+    log = f'We save data in {destFile}\n'
+    with open(destFile, 'w') as f:
+        f.write(response.content.decode())
+    return log
+
+
 if __name__ == '__main__':
+    trainingUrl = 'http://eclecti.cc/files/2008/03/haarcascade_frontalface_alt.xml'
+    if not os.path.exists('/tmp/BHP/training/haarcascade_frontalface_alt.xml'):
+        print(downloadTraining(trainingUrl, '/tmp/BHP/training'))
     detect('/tmp/BHP/pictures', '/tmp/BHP/faces', '/tmp/BHP/training')
